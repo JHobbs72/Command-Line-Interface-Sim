1f10168df7946ff35c08c7c1f1242795a624bc9b
JHobbs72 <jacob.hobbs@virginmedia.com>
2022-07-18 17:13:16 +0100

    cat root command implementation complete

--- Assets/Scripts/CAT.cs
+++ Assets/Scripts/CAT.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using UnityEngine;
 
@@ -7,131 +8,268 @@ public class CAT : MonoBehaviour
 {
 
     public GraphManager fileSystem;
+    private bool _nOption = false;
+    private bool _eOption = false;
+    private bool _bOption = false;
+    private bool _sOption = false;
 
     public void cat(string input)
     {
-        Tuple<char[], string[]> command = fileSystem.SeparateAndValidate(input, "cat", new[] {'n', 'E'}, "Cat Usage...");
+        if (string.IsNullOrEmpty(input)) { fileSystem.SendOutput("Cat usage", false); return; }
+        Tuple<char[], string[]> command = fileSystem.SeparateAndValidate(input, "cat", new[] {'n', 'E', 'b', 's'}, "Cat Usage...");
         if (command == null) { return; }
         
         char[] options = command.Item1;
         string[] arguments = command.Item2;
         
-        
+        if (options != null)
+        {
+            if (options.Contains('E'))
+            {
+                _eOption = true;
+            }
+
+            if (options.Contains('n'))
+            {
+                _nOption = true;
+            }
+            
+            if (options.Contains('b'))
+            {
+                _bOption = true;
+            }
+
+            if (options.Contains('s'))
+            {
+                _sOption = true;
+            }
+        }
+
         if (arguments.Contains("<<"))
         {
-            fileSystem.SendOutput("Error - invalid argument '<<'", false);
+            fileSystem.SendOutput("Error -> invalid argument '<<'", false);
             return;
         }
-        
+
+        if (arguments.Contains("<") || !arguments.Contains(">>") || !arguments.Contains(">") || !arguments.Contains("<"))
+        {
+            WriteToStdOut(arguments);
+        }
+
+        // if (!arguments.Contains(">>") || !arguments.Contains(">") || !arguments.Contains("<"))
+        // {
+        //     WriteToStdOut(arguments);
+        // }
+
         if (arguments.Contains(">") || arguments.Contains(">>"))
         {
             WriteToFile(arguments);
-            return;
         }
+    }
 
-        if (arguments[0] == "<")
+    private FileNode GetNode(string input, bool createNewEndNode)
+    {
+        string[] path = input.Split('/');
+        if (path.Length > 1)
         {
-            WriteToStdOut(arguments);
-            return;
+            List<Node> nodePath = fileSystem.CheckPath(fileSystem.GetCurrentNode(), path, 0, new List<Node>());
+            if (nodePath == null)
+            {
+                List<Node> testPath = fileSystem.CheckPath(fileSystem.GetCurrentNode(), path.SkipLast(1).ToArray(), 0, new List<Node>());
+                if (testPath == null)
+                {
+                    fileSystem.SendOutput("Error - invalid path", false);
+                    return null;
+                }
+
+                // If the target node is a destination and doesn't exist in the path -> create it
+                if (createNewEndNode)
+                {
+                    if (testPath[^1].GetType() == typeof(DirectoryNode))
+                    {
+                        fileSystem.AddFileNode((DirectoryNode)testPath[^1], path[^1]);
+                        return (FileNode)fileSystem.CheckPath(fileSystem.GetCurrentNode(), path, 0, new List<Node>())[^1];
+                    }
+                
+                    fileSystem.SendOutput("Error --> invalid path", false);
+                    return null;
+                }
+                
+                // If the target node is a source and doesn't exist -> error
+                fileSystem.SendOutput("Error --> invalid path", false);
+                return null;
+            }
+
+            if (nodePath[^1].GetType() == typeof(FileNode))
+            {
+                return (FileNode)nodePath[^1];
+            }
+            
+            fileSystem.SendOutput("Error --> is Directory", false);
+            return null;
         }
         
-        if (arguments.Contains("<"))
+        Node target = fileSystem.GetCurrentNode().SearchChildren(path[0]);
+        if (target.GetType() == typeof(FileNode))
         {
-            fileSystem.SendOutput("Error - invalid argument '<'", false);
-            return;
+            return (FileNode)target;
         }
 
-        ShowContents(arguments);
-    }
-
-    private List<Node> GetNodes(string[] input)
-    {
         return null;
-    }
-
-    private void ShowContents(string[] arguments)
-    {
-        List<FileNode> validFiles = new List<FileNode>();
-        foreach (string arg in arguments)
-        {
-            string[] path = arg.Split('/');
-            if (path.Length > 1)
-            {
-                List<Node> nodePath = fileSystem.CheckPath(fileSystem.GetCurrentNode(), path, 0, new List<Node>());
-                if (nodePath != null)
-                {
-                    if (nodePath[^1].GetType() != typeof(FileNode))
-                    {
-                        fileSystem.SendOutput("cat: " + nodePath[^1].name + ": Is a directory", false);
-                    }
-                    else
-                    {
-                        validFiles.Add((FileNode)nodePath[^1]);
-                    }
-                }
-                else
-                {
-                    fileSystem.SendOutput("cat: " + path[^1] + ": No such file or directory", false);
-                }
-            }
-            else
-            {
-                Node file = fileSystem.GetCurrentNode().SearchChildren(arg);
-                if (file != null)
-                {
-                    if (file.GetType() == typeof(FileNode))
-                    {
-                        validFiles.Add((FileNode)file);
-                    }
-                    else
-                    {
-                        fileSystem.SendOutput("cat: " + file.name + ": Is a directory", false);
-                    }
-                }
-                else
-                {
-                    fileSystem.SendOutput("cat: " + arg + ": No such file or directory", false);
-                }
-            }
-        }
-
-        fileSystem.SendOutput("", false);
-        foreach (FileNode node in validFiles)
-        {
-            fileSystem.SendSpecialOutput("\n" + node.GetContents());
-        }
     }
 
     private void WriteToStdOut(string[] arguments)
     {
-        Node nodeContents = fileSystem.GetCurrentNode().SearchChildren(arguments[0]);
-        if (nodeContents != null)
+        List<string> input = arguments.ToList();
+        List<string> content = new List<string>();
+
+        if (input.Contains("<"))
         {
-            if (nodeContents.GetType() == typeof(FileNode))
+            while (input.Contains("<"))
             {
-                fileSystem.SendOutput(nodeContents.GetContents(), false);
-            }
-            else
-            {
-                fileSystem.SendOutput("cat: " + nodeContents.name + ": Is a directory", false);
+                int op = input.IndexOf("<");
+
+                FileNode node = GetNode(input[op + 1], false);
+                if (node == null)
+                {
+                    return;
+                }
+
+                content.Add(node.GetContents());
+
+                input.RemoveAt(op);
+                input.RemoveAt(op);
             }
         }
         else
         {
-            fileSystem.SendOutput("cat: " + arguments[0] + ": No such file or directory", false);
+            foreach (string src in input)
+            {
+                FileNode node = GetNode(src, false);
+                if (node == null)
+                {
+                    return;
+                }
+
+                content.Add(node.GetContents());
+
+            }
         }
+        
+        OutputWithOptions(content);
     }
 
     private void WriteToFile(string[] arguments)
     {
+        List<string> input = arguments.ToList();
+        List<Tuple<FileNode, string>> destinations = new List<Tuple<FileNode, string>>();
+        List<FileNode> sources = new List<FileNode>();
+        string content = "";
         
+        if (input[0] == ">" || input[0] == ">>" || input[^1] == ">" || input[^1] == ">>")
+        {
+            fileSystem.SendOutput("Error --> parse error", false);
+            return;
+        }
+
+        while (input.Contains(">") || input.Contains(">>"))
+        {
+            int op = input.IndexOf(">");
+            if (op < 0)
+            {
+                op = input.IndexOf(">>");
+            }
+
+            FileNode dest = GetNode(input[op + 1], true);
+            if (dest == null)
+            {
+                return;
+            }
+            
+            destinations.Add(new Tuple<FileNode, string>(dest, input[op]));
+            // Remove operand and operator e.g: > file2.txt
+            input.RemoveAt(op);
+            // Indexes change when first removed so same index twice
+            input.RemoveAt(op);
+        }
+        
+        foreach (string target in input)
+        {
+            FileNode source = GetNode(target, false);
+            if (source == null)
+            {
+                fileSystem.SendOutput("Error --> No such file or directory", false);
+                return;
+            }
+            
+            sources.Add(source);
+            if (content == "")
+            {
+                content = source.GetContents();
+            }
+            else
+            {
+                content = content + "\n" + source.GetContents();
+            }
+        }
+
+        foreach (Tuple<FileNode, string> dest in destinations)
+        {
+            if (dest.Item2 == ">")
+            {
+                dest.Item1.SetContents(content);
+            }
+            else
+            {
+                dest.Item1.SetContents(dest.Item1.GetContents() + "\n" + content);
+            }
+        }
+        
+        fileSystem.SendOutput("", false);
     }
 
-    private void OutputWithOptions(string[] output)
+    private void OutputWithOptions(List<string> output)
     {
         // Add line numbers if -n
+        // Add line numbers on non whitespace if -b
         // Add $ at end of file if -E
-        // If not options STD out
-        fileSystem.SendOutput(string.Join(' ', output), false);
+        // Suppress whitespace to max 1 line if -s
+        // If no options STD out
+
+        if (_sOption)
+        {
+            for (int i = 0; i < output.Count; i++)
+            {
+                if (string.IsNullOrEmpty(output[i]) && string.IsNullOrEmpty(output[i + 1]))
+                {
+                    output.RemoveAt(i);
+                    i -= 1;
+                }
+            }
+        }
+
+        if (_eOption)
+        {
+            int count = 1;
+            for (int i = 1; i <= output.Count; i++)
+            {
+                if (string.IsNullOrEmpty(output[i]) && _bOption)
+                {
+                    // Skip
+                }
+                else
+                {
+                    output[i] = count + output[i];
+                    count++;
+                }
+            }
+        }
+
+        if (_nOption)
+        {
+            output[^1] += "$";
+        }
+        
+        fileSystem.SendOutput(string.Join('\n', output), false);
     }
 }
