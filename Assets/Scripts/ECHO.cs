74926c44b7df8d3a0d34041fd31dcd3345d3e822
JHobbs72 <jacob.hobbs@virginmedia.com>
2022-07-27 11:24:28 +0100

    Comments for echo and mkdir

--- Assets/Scripts/ECHO.cs
+++ Assets/Scripts/ECHO.cs
@@ -21,14 +21,18 @@ public class ECHO : MonoBehaviour {
             {
                 // TODO error message
                 fileSystem.SendOutput("Illegal option " + arg, false);
+                // TODO usage
                 fileSystem.SendOutput("usage ...", true);
                 return;
             }
         }
 
+        // Initialise list of tuples to store the name of the file to be written to and whether the text is to append or overwrite
         List<Tuple<string, string>> operatorAndDest = new List<Tuple<string, string>>();
+        // List of elements to be removed = operators and file to be written to - left with just the text to write
         List<int> toRemove = new List<int>();
         
+        // Iterate through the command separating text from operators from destination files
         for (int i = 0; i < arguments.Length; i++)
         {
             if (arguments[i] == ">" || arguments[i] == ">>")
@@ -39,25 +43,31 @@ public class ECHO : MonoBehaviour {
             }
         }
 
+        // Initialize list of operators and destination file nodes
         List<Tuple<string, FileNode>> destinations = new List<Tuple<string, FileNode>>();
 
+        // Iterate through each destination, check the node specified exists and add to destinations list
         foreach (Tuple<string, string> pair in operatorAndDest)
         {
             string[] path = pair.Item2.Split('/');
+            // If the destination is a path
             if (path.Length > 1)
             {
                 List<Node> nodePath = fileSystem.CheckPath(fileSystem.GetCurrentNode(), path, 0, new List<Node>());
                 if (nodePath == null)
                 {
+                    // TODO TEST --> is having 'testPath' right? Looks like if the last node in 'nodePath' is invalid it's ignored and use the second last? Meant to create a new node of the last?
                     List<Node> testPath = fileSystem.CheckPath(fileSystem.GetCurrentNode(), path.SkipLast(1).ToArray(), 0, new List<Node>());
                     if (testPath == null)
                     {
+                        // TODO error message
                         fileSystem.SendOutput("Error - incorrect path", false);
                         return;
                     }
 
                     if (testPath[^1].GetType() != typeof(FileNode))
                     {
+                        // TODO error message
                         fileSystem.SendOutput("Error - incorrect path", false);
                         return;
                     }
@@ -65,8 +75,10 @@ public class ECHO : MonoBehaviour {
                 }
                 else
                 {
+                    // Last node in path must be a file node to write to it
                     if (nodePath[^1].GetType() != typeof(FileNode))
                     {
+                        // TODO error message
                         fileSystem.SendOutput("Error - incorrect path", false);
                         return;
                     }
@@ -75,17 +87,21 @@ public class ECHO : MonoBehaviour {
             }
             else
             {
+                // If the destination is a singel node
                 Node dest = fileSystem.GetCurrentNode().SearchChildren(path[0]);
                 if (dest == null)
                 {
+                    // If the last node doesn't exist one is created before adding to destinations list
                     fileSystem.AddFileNode(fileSystem.GetCurrentNode(), path[0]);
                     Node newFile = fileSystem.GetCurrentNode().SearchChildren(path[0]);
                     destinations.Add(new Tuple<string, FileNode>(pair.Item1, (FileNode)newFile));
                 }
                 else
                 {
+                    // If node exists but is a directory node
                     if (dest.GetType() != typeof(FileNode))
                     {
+                        // TODO error message
                         fileSystem.SendOutput("Error", false);
                         return;
                     }
@@ -94,6 +110,7 @@ public class ECHO : MonoBehaviour {
             }
         }
 
+        // Gather together the text to be written to each file
         List<string> contents = new List<string>();
         for (int i = 0; i < arguments.Length; i++)
         {
@@ -103,6 +120,7 @@ public class ECHO : MonoBehaviour {
             }
         }
 
+        // Write the contents to the relevant files, operators dictate appending or overwriting
         foreach (Tuple<string, FileNode> dest in destinations)
         {
             if (dest.Item1 == ">")
@@ -117,6 +135,7 @@ public class ECHO : MonoBehaviour {
             
         }
         
+        // TODO Correct outputs on error but continue
         fileSystem.SendOutput("", false);
     }
 }
--- Assets/Scripts/MKDIR.cs
+++ Assets/Scripts/MKDIR.cs
@@ -13,57 +13,61 @@ using UnityEngine;
 
 public class MKDIR : MonoBehaviour
 {
+    // Root command for 'make directory' 
+    
     public GraphManager fileSystem;
     private bool _pOption;
     private bool _vOption;
     
     public void mkdir(string input)
     {
-        //     fileSystem.SendOutput("usage: mkdir [-pv] directory ...", false);
-        // TODO Abstraction --> '{' '}' and ',' are allowed in certain conditions
-        // TODO             --> for this purpose it's valid to remove them at all times  
-
         Tuple<char[], string[]> command = fileSystem.SeparateAndValidate(input, "mkdir", new[] { 'p', 'v' }, 
             "usage: mkdir [-pv] directory ...");
+        
         if (command == null) { return; }
         
         char[] charOptions = command.Item1;
         string[] arguments = command.Item2;
         
+        // Remove illegal characters -- Abstraction --> '{' '}' and ',' are allowed in certain conditions. For this purpose it's valid to remove them at all times  
         for (int i = 0; i < arguments.Length; i++)
         {
             arguments[i] = Regex.Replace(arguments[i], @"['\{},']+", "");
         }
 
+        // Set option flags
         if (charOptions != null)
         {
             if (charOptions.Contains('p')) { _pOption = true; }
             if (charOptions.Contains('v')) { _vOption = true; }
         }
         
+        // If no arguments given
         if (arguments.Length == 0)
         {
             fileSystem.SendOutput("usage: mkdir [-pv] directory ...", false);
             return;
         }
 
-
         fileSystem.SendOutput("", false);
         
+        // Iterate through each arguments
         foreach (string arg in arguments)
         {
             string[] path = arg.Split('/');
 
             if (path.Length > 1)
             {
+                // If argument is a path
                 if (_pOption)
                 {
+                    // If arguments is a path and option os given requiring any intermediate, non-existing directories to be created
                     AddParentDirs(fileSystem.GetCurrentNode(), path, 0);
                 }
                 else
                 {
-                    List<Node> validPath = fileSystem.CheckPath(fileSystem.GetCurrentNode(), path.SkipLast(1).ToArray(), 
-                        0, new List<Node>());
+                    // Check the path, if valid create the dir at the end
+                    List<Node> validPath = fileSystem.CheckPath(fileSystem.GetCurrentNode(), path.SkipLast(1).ToArray(), 0, new List<Node>());
                     if (validPath[^1].GetType() == typeof(DirectoryNode))
                     {
                         AddDir((DirectoryNode)validPath[^1], path[^1]);
@@ -72,37 +76,53 @@ public class MKDIR : MonoBehaviour
             }
             else
             {
+                // If a single argument is given (not a path), create under current node
                 AddDir(fileSystem.GetCurrentNode(), path[0]);
             }
         }
     }
 
+    // Recursive method to create a directory from a path, including the creation of any directories that don't already exist in the path given
+        // lcn = (Local current node) the current directory being visited
+        // path = The string path
+        // step = The index of the path being visited 
     private void AddParentDirs(DirectoryNode lcn, string[] path, int step)
     {
+        // Return if at the end of the path
         if (step == path.Length)
         {
             return;
         }
         
+        // Iterate through the local current node's neighbours
         foreach (Node node in lcn.GetNeighbours())
         {
+            // If the next node exists and is a directory node
             if (node.name == path[step] && node.GetType() == typeof(DirectoryNode))
             {
+                // Recursive call to continue along the path
                 AddParentDirs((DirectoryNode)node, path, step + 1);
             }
+            // If the next node exists but is a file - error, invalid path
             else if (node.name == path[step] && node.GetType() == typeof(FileNode))
             {
+                // TODO error message
                 fileSystem.SendOutput("Invalid path", false);
                 return;
             }
+            // The next node doesn't exist, could be the last node in the path or an intermediate node that need to be created before continuing
             else
             {
+                // Recursive call to continue along the path
                 AddParentDirs(AddDir((DirectoryNode)node, path[step]), path, step + 1);
                 break;
             }
         }
     }
 
+    // Method to create a new directory
+        // parent = the directory that the new directory will be a child of
+        // newNode = the name of the directory to be created
     private DirectoryNode AddDir(DirectoryNode parent, string newNode)
     {
         List<Node> neighbours = parent.GetNeighbours();
@@ -110,6 +130,7 @@ public class MKDIR : MonoBehaviour
         {
             if (node.name == newNode)
             {
+                // Check if a node with that name exists already
                 fileSystem.SendOutput("mkdir: " + newNode + ": File exists", false);
                 return null;
             }
