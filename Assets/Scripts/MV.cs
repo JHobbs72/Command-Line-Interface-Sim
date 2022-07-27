dfe1fdca060fec8d15a6044cdc4b21ac6f8204e8
JHobbs72 <jacob.hobbs@virginmedia.com>
2022-07-27 12:05:39 +0100

    Comments for mv and output

--- Assets/Scripts/MV.cs
+++ Assets/Scripts/MV.cs
@@ -11,8 +11,9 @@ using UnityEngine;
 public class MV : MonoBehaviour
 {
     // Root command for 'move' - move a file from one directory to another
+        // Can move, rename a file or overwrite one file with another
 
-    public GraphManager fileSystem;
+        public GraphManager fileSystem;
 
     public void mv(string input)
     {
@@ -20,8 +21,10 @@ public class MV : MonoBehaviour
         Tuple<char[], string[]> command = fileSystem.SeparateAndValidate(input, "mv", new[] {'f', 'i', 'n', 'v'}, 
             "usage: mv [-f | -i | -n] [-v] source target \n" +
             "           mv [-f | -i | -n] [-v] source ... directory");
+        
         if (command == null) { return; }
         
+        // Must have a source and a destination
         if (command.Item2.Length < 2)
         {
             fileSystem.SendOutput("usage: mv [-f | -i | -n] [-v] source target \n" +
@@ -29,34 +32,46 @@ public class MV : MonoBehaviour
             return;
         }
 
+        // Gets and checks the destination - deals with path and returns a tuple with one or both items null
+            // - If the node exists it's returned as item1 and item2 is null
+            // If no node exists, item1 is null, item2 is the string name
         Tuple<Node, string> dest = GetDest(command.Item2[^1]);
         if (dest == null)
         {
+            // Invalid destination
+            // TODO error message
             fileSystem.SendOutput("Error in dest", false);
             return;
         }
 
+        // All but last nodes are a source, check they exist -- 'GetValidSource' deals with paths
         List<Node> validSrcNodes = GetValidSource(command.Item2.SkipLast(1).ToArray());
 
+        // If there's more than one source --> must be wanting to move multiple directories to another directory
+        // TODO TEST -- move multiple FILES and directories?
         if (validSrcNodes.Count > 1)
         {
+            // If the node doesn't exist
             if (dest.Item1 == null)
             {
+                // TODO error message
                 fileSystem.SendOutput("Not a directory 1", false);
                 return;
             }
-
+            // Node should be a directory
             if (dest.Item1.GetType() == typeof(FileNode))
             {
+                // TODO error message
                 fileSystem.SendOutput("Not a directory 2", false);
                 return;
             }
             
+            // Call to move all the valid source nodes
             Move(validSrcNodes, (DirectoryNode)dest.Item1);
             return;
         }
         
-        //if dest == name --> RENAME
+        // If dest == name --> RENAME
         if (dest.Item2 != null)
         {
             Rename(validSrcNodes[0], dest.Item2);
@@ -65,21 +80,21 @@ public class MV : MonoBehaviour
         
         if (dest.Item1 != null)
         {
-            // if <file> and <dir> --> MOVE
+            // if source = <file> and dest = <dir> --> MOVE
             if (validSrcNodes[0].GetType() == typeof(FileNode) && dest.Item1.GetType() == typeof(DirectoryNode))
             {
                 Move(new List<Node> { validSrcNodes[0] }, (DirectoryNode)dest.Item1);
                 return;
             }
             
-            // if <file> and <file> --> OVERWRITE
+            // if source = <file> and dest = <file> --> OVERWRITE
             if (validSrcNodes[0].GetType() == typeof(FileNode) && dest.Item1.GetType() == typeof(FileNode))
             {
                 Overwrite((FileNode)validSrcNodes[0], (FileNode)dest.Item1);
                 return;
             }
             
-            // if <dir> and <dir> --> MOVE
+            // if source = <dir> and dest = <dir> --> MOVE
             if (validSrcNodes[0].GetType() == typeof(DirectoryNode) && dest.Item1.GetType() == typeof(DirectoryNode))
             {
                 Move(new List<Node> { validSrcNodes[0] }, (DirectoryNode)dest.Item1);
@@ -87,13 +102,14 @@ public class MV : MonoBehaviour
         }
     }
 
-    // Returns destination as tuple
-    // one of {<Node>(<dir> or <file>), <name>}
+    // Method to return the destination node as tuple to signify if the destination is an existing node or the name of a node to be created
+        // One of {<Node>, <name>} will be null, the other will be the value --> destination can be a string name (new node) or an existing node
     private Tuple<Node, string> GetDest(string destination)
     {
         string[] dest = destination.Split('/');
         if (dest.Length > 1)
         {
+            // Check path
             List<Node> destPath = fileSystem.CheckPath(fileSystem.GetCurrentNode(), dest, 0, new List<Node>());
             if (destPath == null)
             {
@@ -101,6 +117,7 @@ public class MV : MonoBehaviour
                     0, new List<Node>());
                 if (testPath == null)
                 {
+                    // TODO error message
                     fileSystem.SendOutput("Error - invalid path", false);
                     return null;
                 }
@@ -111,17 +128,21 @@ public class MV : MonoBehaviour
             
         }
         
+        // Go up a directory
         if (dest[0] == "..")
         {
             return new Tuple<Node, string>(fileSystem.GetCurrentPath()[^2], null);
         }
     
+        // If not a path
         Node validNode = fileSystem.GetCurrentNode().SearchChildren(dest[0]);
         
+        // Check if exists and return the relevant tuple
         return validNode == null ? new Tuple<Node, string>(null, dest[0]) : 
             new Tuple<Node, string>(validNode, null);
     }
 
+    // Method to take in a list of potential file or directory names/paths and check if they exist - return all that exist, skip those that don't
     private List<Node> GetValidSource(string[] arguments)
     {
         List<Node> validSrcNodes = new List<Node>();
@@ -132,9 +153,11 @@ public class MV : MonoBehaviour
 
             if (path.Length > 1)
             {
+                // Check path
                 List<Node> validPath = fileSystem.CheckPath(fileSystem.GetCurrentNode(), path, 0, new List<Node>());
                 if (validPath == null)
                 {
+                    // TODO error message
                     fileSystem.SendOutput("Invalid source 1", false);
                 }
                 else
@@ -144,9 +167,11 @@ public class MV : MonoBehaviour
             }
             else
             {
+                // Not a path, check and add if exists
                 Node target = fileSystem.GetCurrentNode().SearchChildren(path[0]);
                 if (target == null)
                 {
+                    // TODO error message
                     fileSystem.SendOutput("Invalid source 2", false);
                 }
                 else
@@ -159,6 +184,7 @@ public class MV : MonoBehaviour
         return validSrcNodes;
     }
 
+    // Method to rename a node
     private void Rename(Node src, string dest)
     {
         src.name = dest;
@@ -166,6 +192,8 @@ public class MV : MonoBehaviour
         fileSystem.SendOutput("", false);
     }
 
+    // Method to overwrite one file with another
+    // TODO reset contents?
     private void Overwrite(FileNode src, FileNode dest)
     {
         // Dest takes contents of src, src removed
@@ -175,6 +203,7 @@ public class MV : MonoBehaviour
         fileSystem.SendOutput("", false);
     }
 
+    // Method to move multiple nodes to a new directory
     private void Move(List<Node> srcList, DirectoryNode dest)
     {
 
--- Assets/Scripts/outputText.cs
+++ Assets/Scripts/outputText.cs
@@ -8,7 +8,7 @@ using UnityEngine;
 
 public class outputText : MonoBehaviour
 {
-    // Adds output from system to user's 'display'
+    // Adds output from system to standard output (user's display)
 
     private TextMeshProUGUI _output;
     private prompt _prompt;
@@ -19,6 +19,10 @@ public class outputText : MonoBehaviour
         _prompt = FindObjectOfType<prompt>();
     }
 
+    // Method called from GraphManager.cs
+        // command = The full command the user entered
+        // content = The result of executing that command to be displayed
+        // flag = Boolean value dictating whether or not to display the command -- more flexibility 
     public void AddOutput(string command, string content, bool flag)
     {
         if (content.Length == 0 && !flag)
@@ -37,11 +41,14 @@ public class outputText : MonoBehaviour
         _prompt.UpdatePrompt();
     }
     
+    // Method called from GraphManager.cs
+        // Directly outputs content to standard out without any command or '>>' prompt for full flexibility of output
     public void SpecialOutput(string content)
     {
         _output.text += content;
     }
 
+    // Called after the command has been entered to empty the text box ready for the next command
     public void EmptyOut()
     {
         _output.text += "\n>>";
